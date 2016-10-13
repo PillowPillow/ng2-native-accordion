b7fd19c3f480ad32e9cdb1c6d7e0368f2012e5f5
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2016-10-13 06:38:11 +0200

    add config

---
+++ config/rollup.js
@@ -0,0 +1,11 @@
+export default {
+  entry: 'index.js',
+  dest: 'bundles/core.umd.js',
+  format: 'umd',
+  sourceMap: 'inline',
+  moduleName: 'ng2Webstorage',
+  globals: {
+    '@angular/core': 'ng.core',
+    '@angular/common': 'ng.common'
+  }
+};
